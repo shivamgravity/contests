ea928989c48df659404a6d99e271d8e852b2552b
Shivam Gravity <shivamgravity@gmail.com>
2025-08-21 23:37:28 +0530

    added new answer

--- codeforces/div 3/B_the_secret_number.py
+++ codeforces/div 3/B_the_secret_number.py
@@ -1,20 +1,23 @@
 def main():
     # code
-    n = int(input())
-    for _ in range(n):
-        num = int(input())
-        L = []
-        for x in range(11,num):
-            temp = x*10
-            while temp < num:
-                if x + temp == num:
-                    L.append(x)
-                temp *= 10
-        print(len(L))
-        L.sort()
-        for x in L:
-            print(x, end=" ")
-        print()
+    test_cases = int(input())
+    for _ in range(test_cases):
+        n = int(input())
+        a = 1
+        res = []
+        while n // (1 + 10**a) >:
+            if n % (1 + 10**a) != 0:
+                a += 1
+                continue
+            res.append(n // (1 + 10**a))
+            a += 1
+        print(len(res))
+        if len(res) > 0:
+            res.sort()
+            for x in res:
+                print(x, end=" ")
+            print()
 
 if __name__ == '__main__':
-    main()
+    main() 
+    
